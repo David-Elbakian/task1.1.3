d059f2edd6b48b3297a80aae21dda0bab2a737ad
DavidElbakian555 <136856060+DavidElbakian555@users.noreply.github.com>
2023-06-30 15:45:15 +0400

    task1.1.3 Реализовал соединения с БД в классе Util Реализовал методы в UserDao Запускаю через UserService

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static final String DRIVER = "org.postgresql.Driver";
+    public static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    public static final String USER = "postgres";
+    public static final String PASSWORD = "1";
+
+
+
+    public static Connection getConnection() {
+        Connection connection = null;
+
+        try {
+
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            if (connection != null) {
+                System.out.println("Connected success");
+            } else {
+                System.out.println("Connection failed");
+            }
+        } catch (SQLException  | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 }
